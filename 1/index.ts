27e14d548fb4e636a3beda06a31ddd2ccd6f7ee4
Simon Busard <sbu@n-side.com>
2022-12-02 16:41:57 +0100

    Day 1: code clean up

--- 1/index.ts
+++ 1/index.ts
@@ -1,20 +1,19 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
 
-function extractBlocks(input: string) {
-    const blocks = [[]];
-    const split = input.split('\n\n');
-    return split.map((block) => block.split('\n').map((f) => Number(f)));
+function extractBlocks(input: string): Array<Array<number>> {
+    const blocks = input.split('\n\n');
+    return blocks.map((block) => block.split('\n').map((f) => Number(f)));
 }
 
-function part1(inputPath: string) {
+function part1(inputPath: string): number | undefined {
     const input = readFileSync(inputPath, 'utf8');
     const blocks = extractBlocks(input);
     const calories = blocks.map((block) => _.sum(block));
     return _.max(calories);
 }
 
-function part2(inputPath: string) {
+function part2(inputPath: string): number {
     const input = readFileSync(inputPath, 'utf8');
     const blocks = extractBlocks(input);
     const calories = blocks.map((block) => _.sum(block));
@@ -22,4 +21,5 @@ function part2(inputPath: string) {
     return _.sum(sortedCalories.slice(0, 3));
 }
 
-console.log(part2('input/input.txt'));
+console.log("Part 1: ", part1('input/input.txt'));
+console.log("Part 2: ", part2('input/input.txt'));
